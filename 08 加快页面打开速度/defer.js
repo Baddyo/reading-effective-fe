bcf8ef5b0be1edd1400bf4a1c7b9d7686f554c93
Baddyo <qianjunyinggo@gmail.com>
2020-09-09 22:16:22 +0800

    完成第三章

---
+++ 08 加快页面打开速度/defer.js
@@ -0,0 +1,5 @@
+console.log('Defer start');
+for (let i = 0; i < 10000; i++) {
+    new Date();
+}
+console.log('Defer end');
---
+++ 08 加快页面打开速度/normal.js
@@ -0,0 +1,5 @@
+console.log('Normal start');
+for (let i = 0; i < 10000; i++) {
+    new Date();
+}
+console.log('Normal end');
